de6c8e23d388c7c50cf165ea9374261854172b1f
Panjks <panjks@163.com>
2018-03-16 23:17:05 +0800

    Finish Algorithms_15

---
+++ Python 3/Algorithm_15.py
@@ -0,0 +1,75 @@
+"""
+Problem: 3SUM
+
+
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+Find all unique triplets in the array which gives the sum of zero.
+
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+"""
+
+
+class SolutionFailedTimeOut:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        for i in range(0, len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        temp = [nums[i], nums[j], nums[k]]
+                        result.append(temp)
+        # 除重算法
+        temp = []
+        for i in result:
+            i.sort()
+            if i not in temp:
+                temp.append(i)
+        result = temp
+        return result
+
+
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = []
+        i = 0
+        while i < len(nums) - 2:
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                if nums[j] + nums[k] + nums[i] == 0:
+                    temp = [nums[i], nums[j], nums[k]]
+                    result.append(temp)
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+                elif nums[j] + nums[k] + nums[i] < 0:
+                    j += 1
+                else:
+                    k -= 1
+            i = i + 1
+            while i < len(nums) - 2 and nums[i] == nums[i - 1]:
+                i += 1
+        return result
+
+if __name__ == "__main__":
+    sln = Solution()
+    test_nums = [-1, 0, 1, 2, -1, -4]
+    print(sln.threeSum(test_nums))
+    # testing
